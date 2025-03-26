7ff0d50c4d7d0cbf514995b01462c694fa64eff8
Swathi <swathi@datazip.io>
2025-03-26 16:18:20 +0530

    perf: :goal_net: Add Error Boundary which helps to catch errors and show fallback ui instead of crash

--- olake_frontend/src/App.tsx
+++ olake_frontend/src/App.tsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom"
 import { ConfigProvider, App as AntApp, Spin } from "antd"
 import Layout from "./modules/common/components/Layout"
+import { ErrorBoundary } from "./modules/common/components/ErrorBoundary"
 
 // Lazy load components
 const Jobs = lazy(() => import("./modules/jobs/pages/Jobs"))
@@ -50,79 +51,81 @@ function App() {
 			}}
 		>
 			<AntApp>
-				<Router>
-					<Layout>
-						<Suspense fallback={<LoadingFallback />}>
-							<Routes>
-								<Route
-									path="/jobs"
-									element={<Jobs />}
-								/>
-								<Route
-									path="/jobs/new"
-									element={<JobCreation />}
-								/>
-								<Route
-									path="/jobs/:jobId/edit"
-									element={<JobEdit />}
-								/>
-								<Route
-									path="/jobs/:jobId/history"
-									element={<JobHistory />}
-								/>
-								<Route
-									path="/jobs/:jobId/history/:historyId/logs"
-									element={<JobLogs />}
-								/>
-								<Route
-									path="/jobs/:jobId/settings"
-									element={<JobSettings />}
-								/>
-								<Route
-									path="/sources"
-									element={<Sources />}
-								/>
-								<Route
-									path="/sources/new"
-									element={<CreateSource />}
-								/>
-								<Route
-									path="/sources/:sourceId"
-									element={<SourceEdit />}
-								/>
-								<Route
-									path="/sources/:sourceId/jobs"
-									element={<SourceJobs />}
-								/>
-								<Route
-									path="/destinations"
-									element={<Destinations />}
-								/>
-								<Route
-									path="/destinations/new"
-									element={<CreateDestination />}
-								/>
-								<Route
-									path="/destinations/:destinationId"
-									element={<DestinationEdit />}
-								/>
-								<Route
-									path="/destinations/:destinationId/jobs"
-									element={<DestinationJobs />}
-								/>
-								<Route
+				<ErrorBoundary>
+					<Router>
+						<Layout>
+							<Suspense fallback={<LoadingFallback />}>
+								<Routes>
+									<Route
+										path="/jobs"
+										element={<Jobs />}
+									/>
+									<Route
+										path="/jobs/new"
+										element={<JobCreation />}
+									/>
+									<Route
+										path="/jobs/:jobId/edit"
+										element={<JobEdit />}
+									/>
+									<Route
+										path="/jobs/:jobId/history"
+										element={<JobHistory />}
+									/>
+									<Route
+										path="/jobs/:jobId/history/:historyId/logs"
+										element={<JobLogs />}
+									/>
+									<Route
+										path="/jobs/:jobId/settings"
+										element={<JobSettings />}
+									/>
+									<Route
+										path="/sources"
+										element={<Sources />}
+									/>
+									<Route
+										path="/sources/new"
+										element={<CreateSource />}
+									/>
+									<Route
+										path="/sources/:sourceId"
+										element={<SourceEdit />}
+									/>
+									<Route
+										path="/sources/:sourceId/jobs"
+										element={<SourceJobs />}
+									/>
+									<Route
+										path="/destinations"
+										element={<Destinations />}
+									/>
+									<Route
+										path="/destinations/new"
+										element={<CreateDestination />}
+									/>
+									<Route
+										path="/destinations/:destinationId"
+										element={<DestinationEdit />}
+									/>
+									<Route
+										path="/destinations/:destinationId/jobs"
+										element={<DestinationJobs />}
+									/>
+									<Route
 									path="*"
-									element={
-										<Navigate
-											to="/jobs"
-											replace
-										/>
-									}
-								/>
-							</Routes>
-						</Suspense>
-					</Layout>
-				</Router>
+										element={
+											<Navigate
+												to="/jobs"
+												replace
+											/>
+										}
+									/>
+								</Routes>
+							</Suspense>
+						</Layout>
+					</Router>
+				</ErrorBoundary>
 			</AntApp>
 		</ConfigProvider>
 	)
