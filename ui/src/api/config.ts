2c1575ca57c6ba78dd842bebcee08e8de9aaaa52
vikash390 <vikash@datazip.io>
2025-08-05 16:03:03 +0530

    fix: react env varible

--- ui/src/api/config.ts
+++ ui/src/api/config.ts
@@ -2,7 +2,7 @@ export const API_CONFIG = {
 	// BASE_URL determines where API requests go.
 	// This uses the same protocol, host, and port that the frontend was loaded from.
 	// Example: if the app is served from http://localhost:8000, requests go to http://localhost:8000/api/...
-	BASE_URL: process.env.IS_DEV  ?`${window.location.protocol}//${window.location.host.split(":")[0]}:8000`:`${window.location.protocol}//${window.location.host}`,
+	BASE_URL: import.meta.env.VITE_IS_DEV === "true"?`${window.location.protocol}//${window.location.host.split(":")[0]}:8000`:`${window.location.protocol}//${window.location.host}`,
 	PROJECT_ID: "123",
 	ENDPOINTS: {
 		PROJECT: (projectId: string) => `/api/v1/project/${projectId}`,
