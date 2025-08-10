6746d5a062598d921f3c09e43a6b9d04364d9271
Taraka Swathi <swathi@datazip.io>
2025-08-10 12:46:53 +0530

    fix: :bug: catalog issue fix (#143)
    * fix: :bug: catalog issue fix
    
    * fix: :bug: catalog issue in create destination flow and set connector in none when updated to s3

--- ui/src/modules/destinations/pages/DestinationEdit.tsx
+++ ui/src/modules/destinations/pages/DestinationEdit.tsx
@@ -27,7 +27,7 @@ import TestConnectionModal from "../../common/Modals/TestConnectionModal"
 import { connectorOptions } from "../components/connectorOptions"
 import EntityEditModal from "../../common/Modals/EntityEditModal"
 import { getStatusIcon } from "../../../utils/statusIcons"
-import { catalogOptions } from "../../../utils/constants"
+import { CATALOG_TYPES, catalogOptions } from "../../../utils/constants"
 
 const DestinationEdit: React.FC<DestinationEditProps> = ({
 	fromJobFlow = false,
@@ -256,6 +256,8 @@ const DestinationEdit: React.FC<DestinationEditProps> = ({
 								setFormData(initialFormData)
 							}
 						}
+					} else {
+						setFormData({})
 					}
 				} else {
 					console.error("Failed to get destination spec:", response.message)
@@ -393,6 +395,11 @@ const DestinationEdit: React.FC<DestinationEditProps> = ({
 		setSchema(null)
 		setUiSchema(null)
 		setConnector(value)
+		if (value === "Apache Iceberg") {
+			setCatalog(CATALOG_TYPES.AWS_GLUE)
+		} else {
+			setCatalog(CATALOG_TYPES.NONE)
+		}
 
 		if (onFormDataChange) {
 			onFormDataChange({})
