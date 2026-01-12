324e292f6c54cb5f5fade9882f1f5012bf67ae78
deepanshupal09-datazip <deepanshu@datazip.io>
2026-01-12 15:52:02 +0530

    feat: add db2 as source

--- ui/src/utils/utils.ts
+++ ui/src/utils/utils.ts
@@ -26,6 +26,7 @@ import Oracle from "../assets/Oracle.svg"
 import AWSS3 from "../assets/AWSS3.svg"
 import ApacheIceBerg from "../assets/ApacheIceBerg.svg"
 import Kafka from "../assets/Kafka.svg"
+import DB2 from "../assets/DB2.svg"
 
 // Normalizes old connector types to their current internal types
 export const normalizeConnectorType = (connectorType: string): string => {
@@ -65,6 +66,8 @@ export const getConnectorImage = (connector: string) => {
 			return Kafka
 		case "s3":
 			return AWSS3
+		case "db2":
+			return DB2
 		default:
 			// Default placeholder
 			return MongoDB
@@ -147,6 +150,8 @@ export const getConnectorInLowerCase = (connector?: string | null) => {
 			return "mysql"
 		case "oracle":
 			return "oracle"
+		case "db2":
+			return "db2"
 		default:
 			return lowerConnector
 	}
@@ -201,6 +206,8 @@ export const getConnectorLabel = (type: string): string => {
 		case "s3":
 		case "S3":
 			return "S3"
+		case "db2":
+			return "DB2"
 		default:
 			return "MongoDB"
 	}
