22f95bf369ae4db320bb423e9f703ea6dcb92b3d
badalprasadsingh <badal@datazip.io>
2026-03-21 15:37:41 +0530

    fix: minor
    Signed-off-by: badalprasadsingh <badal@datazip.io>

--- server/internal/services/compaction/models/utils.go
+++ server/internal/services/compaction/models/utils.go
@@ -3,7 +3,7 @@ package models
 const APIBase = "/api/ams/v1/"
 
 // given the fact, we have only one optimizer group for fusion in V0
-const OptimizerGroup = "spark-cluster"
+const OptimizerGroup = "spark-container"
 
 // hard-coding to S3 now, as the other options are "hadoop" & "OSS" for Compaction
 // GCS & ADLS are supported, given the catalog manages the sdk (eg, Lakekeeper with GCS flavour)
