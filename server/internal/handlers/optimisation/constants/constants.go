51434d017dad9dc5292028117092f16782bd702d
badalprasadsingh <badal@datazip.io>
2026-03-24 11:45:45 +0530

    fix: remove OptimizerGroup from constants
    Signed-off-by: badalprasadsingh <badal@datazip.io>

--- server/internal/handlers/optimisation/constants/constants.go
+++ server/internal/handlers/optimisation/constants/constants.go
@@ -1,8 +1,5 @@
 package constants
 
-// given the fact, we have only one optimizer group for fusion in V0
-const OptimizerGroup = "spark-container"
-
 // hard-coding to S3 now, as the other options are "hadoop" & "OSS" for optimisation
 // GCS & ADLS are supported, given the catalog manages the sdk (eg, Lakekeeper with GCS flavour)
 const DefaultStroageType = "S3"
