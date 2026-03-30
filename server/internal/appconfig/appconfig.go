a2bfb45c48d12bd7ac9a6bed8e46bff478402c59
vishalm0509 <vishal@datazip.io>
2026-03-30 13:04:40 +0530

    chore: cleanup appconfig

--- server/internal/appconfig/appconfig.go
+++ server/internal/appconfig/appconfig.go
@@ -36,12 +36,12 @@ func Load() Config {
 func loadConfig() Config {
 	v := viper.New()
 
-	// Note: config priority: env -> file -> default
+	// Note: config priority: env variables -> file (app.yaml) -> default values
+
 	// set default values
 	v.SetDefault("APP_NAME", "olake-server")
 	v.SetDefault("HTTP_PORT", "8000")
 	v.SetDefault("RUN_MODE", "dev")
-	v.SetDefault("COPY_REQUEST_BODY", true)
 	v.SetDefault("MAX_MEMORY", int64(67108864))
 	v.SetDefault("MAX_UPLOAD_SIZE", int64(67108864))
 	v.SetDefault("OLAKE_POSTGRES_USER", "temporal")
