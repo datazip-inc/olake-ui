8ab7bbaceaaa33cc34d2d188039489d6c87472c3
Ashi Sharma <ashi@datazip.io>
2026-07-24 14:28:52 +0530

    Capitalised event names

--- server/internal/utils/telemetry/constants.go
+++ server/internal/utils/telemetry/constants.go
@@ -19,6 +19,6 @@ const (
 	EventDestinationCreated  = "destination_created"
 	EventSourcesUpdated      = "sources_updated"
 	EventDestinationsUpdated = "destinations_updated"
-	EventInstalledFusion     = "installed_fusion"
-	EventInstallationMode    = "installation_mode"
+	EventInstalledFusion     = "Installed_Fusion"
+	EventInstallationMode    = "Installation_Mode"
 )
